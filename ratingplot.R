df53446907ca115a28270e259941861e4bc058cf
Yash Raj Sinha <yrs2@uw.edu>
2019-11-17 13:55:23 -0800

    made index and plot file

---
+++ ratingplot.R
@@ -0,0 +1,8 @@
+library(ggplot2)
+city_neighborhood_ratings <- function(city_dataset) {
+  data <- city_dataset
+  # group by neighbourhood
+  # get rating column
+  # distance to seattle column
+  # graph x and y scatter with distance to seattle downtown as x increases
+}
