ea7902560e41919a1ee76187e972b587b4610d96
sharisung <56564128+sharisung@users.noreply.github.com>
2019-11-18 14:07:21 -0800

    Update analysis.R

--- scripts/analysis.R
+++ scripts/analysis.R
@@ -1,10 +1,11 @@
-# Seattle 
+# Seattle
 library(dplyr)
 library(ggplot2)
 library("plotly")
 library(leaflet)
 
-seattle_big_listing <- read.csv("data/big_seattle_listings.csv",stringsAsFactors = FALSE)
+seattle_big_listing <- read.csv("data/big_seattle_listings.csv",
+                                stringsAsFactors = FALSE)
 
 best_hosts_near_pike <-
     seattle_big_listing %>%
@@ -12,7 +13,7 @@ best_hosts_near_pike <-
            host_response_rate == "100%",
            host_response_time == "within an hour",
            host_identity_verified == "t",
-           host_has_profile_pic == 't',
+           host_has_profile_pic == "t",
            zipcode == "98101") %>%
     top_n(15, wt = review_scores_rating) %>%
     arrange(-review_scores_rating) %>%
