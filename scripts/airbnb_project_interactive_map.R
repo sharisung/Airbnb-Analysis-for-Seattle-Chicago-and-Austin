095bdbb598e85489ee76ceb406dcf15a7e4b8e38
sharisung <56564128+sharisung@users.noreply.github.com>
2019-11-18 21:41:26 -0800

    Update airbnb_project_interactive_map.R

--- scripts/airbnb_project_interactive_map.R
+++ scripts/airbnb_project_interactive_map.R
@@ -3,20 +3,22 @@ library(dplyr)
 library(ggplot2)
 library("plotly")
 library(leaflet)
-seattle_big_listing <- read.csv("data/big_seattle_listings.csv",
-                                stringsAsFactors = FALSE)
-
-# create the function
-aggregated_function <- function(seattle_big_listing) {
-best_hosts_near_pike <-
-    seattle_big_listing %>%
-    filter(host_is_superhost == "t",
-           host_response_rate == "100%",
-           host_response_time == "within an hour",
-           host_identity_verified == "t",
-           host_has_profile_pic == "t",
-           zipcode == "98101") %>%
-    top_n(15, wt = review_scores_rating) %>%
-    arrange(-review_scores_rating) %>%
-    select(host_name, name, review_scores_rating)
+# Interactive Map
+seattle_listing <- read.csv("data/seattle_listings.csv",
+                            stringsAsFactors = FALSE)
+interactive_map_function <- function(seattle_listing){
+map <- leaflet(seattle_listing) %>%
+    addTiles() %>%
+    addProviderTiles("CartoDB.Positron") %>%
+    addCircles(
+        lat = ~latitude,
+        lng = ~longitude,
+        stroke = FALSE,
+        popup = ~paste("Host name:", host_name, "<br>",
+                       "Listing name:", name, "<br>",
+                       "Price: $", price, "per night", "<br>",
+                       "Room Type:", room_type, "<br>"),
+        radius = ~0.5,
+        fillOpacity = 0.5,
+    )
 }
